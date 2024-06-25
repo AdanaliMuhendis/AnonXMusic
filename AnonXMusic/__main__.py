a1df8e1fc6182e82d1eda0c3e82288c54291c921
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-25 18:25:12 +0300

    Update __main__.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -43,7 +43,7 @@ async def init():
         await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AnonXMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\channel.\n\nYαყıɳ Bιƚƚι🌪..."
         )
         exit()
     except:
@@ -55,7 +55,7 @@ async def init():
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
+    LOGGER("AnonXMusic").info("Yαყıɳ Bιƚƚι🌪...")
 
 
 if __name__ == "__main__":
